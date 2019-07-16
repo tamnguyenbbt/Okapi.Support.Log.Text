5fd49de5e4989560a2a881963575e268b1a705e5
tamnguyenbbt@gmail.com <tamnguyenbbt@gmail.com>
2019-07-16 21:31:26 +1000

    First commit

---
+++ Okapi.Support.Log.Text/Logger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Okapi.Common;
+using Okapi.Logs;
+using Serilog;
+using SeriLogLogger = Serilog.Core.Logger;
+
+namespace Okapi.Support.Log.Text
+{
+    public class Logger : IOkapiLogger
+    {
+        private static SeriLogLogger logger;
+
+        public Logger()
+        {
+            string logFileName = Session.Instance.LogPath;
+            logger = new LoggerConfiguration().WriteTo.File(logFileName).CreateLogger();
+        }
+
+        public void Error(string messageTemplate)
+        {
+            logger.Error(messageTemplate);
+        }
+
+        public void Error(string messageTemplate, Exception exception)
+        {
+            logger.Error(exception, messageTemplate);
+        }
+
+        public void Info(string messageTemplate)
+        {
+            logger.Information(messageTemplate);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,24 @@
+# Okapi.Support.Log.Text
+Support Okapi Web UI automation test library, providing text logging
+
+## NuGet
+* https://www.nuget.org/packages/Okapi.Support.Log.Text/1.0.0
+* Install-Package Okapi.Support.Log.Text -Version 1.0.0
+
+## Dependencies
+### .NETFramework 4.5
+* Okapi.Common (>= 1.0.3)
+* Serilog (>= 2.8.0)
+* Serilog.Sinks.File (>= 4.0.0)
+
+### .NETFramework 4.6
+* Okapi.Common (>= 1.0.3)
+* Serilog (>= 2.8.0)
+* Serilog.Sinks.File (>= 4.0.0)
+          
+## Versions
+* Version **1.0.0** released on 16/07/2019
+
+## Author
+###  **Tam Nguyen**
+[![View My profile on LinkedIn](https://static.licdn.com/scds/common/u/img/webpromo/btn_viewmy_160x33.png)](https://www.linkedin.com/in/tam-nguyen-a0792930/)
